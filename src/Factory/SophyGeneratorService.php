812c1adb09c48c18146620dedee43466b0836680
GhianCo <ghiancorx@gmail.com>
2023-01-13 17:07:09 -0500

    cambios

--- src/Factory/SophyGeneratorService.php
+++ src/Factory/SophyGeneratorService.php
@@ -111,17 +111,20 @@ class SophyGeneratorService
         $__configRepositories .= PHP_EOL;
 
 
-        $__configRepositories .= 'use DI\ContainerBuilder'. PHP_EOL;
+        $__configRepositories .= 'use DI\ContainerBuilder;' . PHP_EOL;
+
         foreach ($this->allTables as $index => $table) {
             $__configRepositories .= "use App\\" . ucfirst($index) . "\Domain\\" . ucfirst($index) . "Repository;" . PHP_EOL;
             $__configRepositories .= "use App\\" . ucfirst($index) . "\Infrastructure\\" . ucfirst($index) . "RepositoryMysql;" . PHP_EOL;
         }
 
-        $__configRepositories .= 'return function (ContainerBuilder $containerBuilder) {'. PHP_EOL;
-        $__configRepositories .= '    $containerBuilder->addDefinitions(['. PHP_EOL;
-        $__configRepositories .= '        ' . ucfirst($index) . 'Repository::class => \DI\autowire(' . ucfirst($index) . 'RepositoryMysql::class)->method(\'setTable\', \'' . ucfirst($index) . '\'),'. PHP_EOL;
-        $__configRepositories .= '    ]);'. PHP_EOL;
-        $__configRepositories .= '};'. PHP_EOL;
+        $__configRepositories .= 'return function (ContainerBuilder $containerBuilder) {' . PHP_EOL;
+        $__configRepositories .= '    $containerBuilder->addDefinitions([' . PHP_EOL;
+        foreach ($this->allTables as $index => $table) {
+            $__configRepositories .= '        ' . ucfirst($index) . 'Repository::class => \DI\autowire(' . ucfirst($index) . 'RepositoryMysql::class)->method(\'setTable\', \'' . ucfirst($index) . '\'),' . PHP_EOL;
+        }
+        $__configRepositories .= '    ]);' . PHP_EOL;
+        $__configRepositories .= '};' . PHP_EOL;
 
         $__configRepositories .= PHP_EOL;
 
